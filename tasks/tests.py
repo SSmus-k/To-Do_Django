c5b5e5038a99caaf6c0035d341ebbaff77fe5777
SSmus-k <sujanbhusal.34@gmail.com>
2026-01-05 14:52:29 +0545

    Added procfile

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    def test_task_creation(self):
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpass"
+        )
+
+        task = Task.objects.create(
+            user=user,
+            title="Test Task",
+            estimated_minutes=30
+        )
+
+        self.assertEqual(task.title, "Test Task")
+        self.assertEqual(task.user.username, "testuser")
